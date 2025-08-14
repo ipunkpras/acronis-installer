b81675fc453b384b67e1617aac4a716bc0f61762
Ipunk Ade Prasetyo <43496743+ipunkpras@users.noreply.github.com>
2025-08-14 13:29:21 +0700

    Update installer-acronis.sh

--- installer-acronis.sh
+++ installer-acronis.sh
@@ -370,7 +370,7 @@ display_menu() {
     echo "║-------------------------------------------------------║"
     echo "║ Docs : https://github.com/ipunkpras/acronis-installer ║"
     echo "║ Org. : Dcloud @PT. Datacomm Diangraha🏢               ║"
-    echo "║---- v1.0 |     🌐 ipunkpras.my.id   | JKT,ID 2025 ----║"
+    echo "║---- v1.0 |     🌐 dcloud.co.id      | JKT,ID 2025 ----║"
     echo "╚═══════════════════════════════════════════════════════╝"
     echo ""
     echo "Choose Action:"
