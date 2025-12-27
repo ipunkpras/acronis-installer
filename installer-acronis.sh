0375432c5d87b21b83256814934fa6d59aff1b93
Ipunk Ade Prasetyo <43496743+ipunkpras@users.noreply.github.com>
2025-12-27 07:12:08 +0700

    Update installer-acronis.sh

--- installer-acronis.sh
+++ installer-acronis.sh
@@ -113,7 +113,7 @@ install_agent() {
   log_msg "Token accepted (hidden)"
 
   # 3. Folder & nama file
-  TMP=${TMP_DIR:-~/acronis-installer}
+  TMP=${TMP_DIR:-/tmp/acronis-installer}
   mkdir -p "$TMP"
   BIN=$TMP/Backup_Agent_for_Linux_x86_64.bin
   URL="https://cloudbackup.datacomm.co.id/download/u/baas/4.0/$VERSION/$BIN"
